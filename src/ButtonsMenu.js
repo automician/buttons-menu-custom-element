9198532101ac32b6a1ba087d42c89e219102c1e2
yashaka <yashaka@gmail.com>
2023-01-31 21:30:48 +0200

    works but without rendering on first render

--- src/ButtonsMenu.js
+++ src/ButtonsMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Style.scss'
 
 export default props => {
@@ -9,41 +9,83 @@ export default props => {
   const valuesList = attributes['values'].value.split(',')
   const maybeAskedDefaultValue = attributes.default?.value
 
-  const elementsToChange = document.querySelectorAll(selectorOfElementsToChange)
-  const maybeHostDefaultValue =
-    elementsToChange && elementsToChange[0].getAttribute(attributeToChange)
-  const defaultValue =
-    maybeHostDefaultValue || maybeAskedDefaultValue || valuesList[0]
-  document
-    .querySelectorAll(selectorOfElementsToChange)
-    .forEach(element => element.setAttribute(attributeToChange, defaultValue))
-
   const storage = {
     _scopedAttributeNameToStore: `automician.ButtonsMenu.${selectorOfElementsToChange}.${attributeToChange}`,
-    getAttributeValue() {
+    getAttributeToChangeValue() {
       return window.localStorage.getItem(this._scopedAttributeNameToStore)
     },
-    setAttributeToChange(value) {
+    setAttributeToChangeValue(value) {
       window.localStorage.setItem(this._scopedAttributeNameToStore, value)
     },
   }
 
-  if (!storage.getAttributeValue()) {
-    storage.setAttributeToChange(defaultValue)
+  if (!storage.getAttributeToChangeValue()) {
+    const elementsToChange = [
+      ...document.querySelectorAll(selectorOfElementsToChange),
+    ]
+    console.log('elementsToChange', elementsToChange)
+
+    const [hostsAttributeSamePrefilledValue] = elementsToChange.reduce(
+      (sameValue, host) => {
+        const areEqual = (arr1, arr2) =>
+          JSON.stringify(arr1) === JSON.stringify(arr2)
+        const sameValueWasCompromised = areEqual(sameValue, [])
+        if (sameValueWasCompromised) {
+          return sameValue
+        }
+        const hostDefault = host.getAttribute(attributeToChange)
+        const sameValueWasSet = sameValue && sameValue.length > 0
+        if (!sameValueWasSet) {
+          // set sameValue first time!
+          return [hostDefault]
+        }
+
+        if (sameValueWasSet && areEqual(sameValue, [hostDefault])) {
+          // compromised!
+          return []
+        }
+
+        return [hostDefault]
+      },
+      null, // default indicator of sameValue, i.e. => sameValueWasNotSet
+    )
+
+    const defaultValue =
+      hostsAttributeSamePrefilledValue || maybeAskedDefaultValue || valuesList[0]
+
+    storage.setAttributeToChangeValue(defaultValue)
   }
 
-  const valueFromStorage = storage.getAttributeValue()
+  const defaultValueFromStorage = storage.getAttributeToChangeValue()
 
+  const changeElementsAttribute = value =>
+    document.querySelectorAll(selectorOfElementsToChange).forEach(element => {
+      if (element.hasAttribute(attributeToChange)) {
+        console.log('has attribute: ', attributeToChange)
+        element[attributeToChange] = value
+      } else {
+        console.log('has no attribute: ', attributeToChange)
+        element.setAttribute(attributeToChange, value)
+      }
+    })
+
+  changeElementsAttribute(defaultValueFromStorage)
+
+  const valueFromStorage = storage.getAttributeToChangeValue()
   const [selectedValue, setSelectedValue] = useState(valueFromStorage)
   const state = { selectedValue, setSelectedValue }
 
-  function changeAttributeValue(value) {
-    document.querySelectorAll(selectorOfElementsToChange).forEach(element => {
-      element.setAttribute(attributeToChange, value)
-    })
-    state.setSelectedValue(value)
+  // useEffect(() => {
+  //   document.querySelectorAll(selectorOfElementsToChange).forEach(element => {
+  //     element.setAttribute(attributeToChange, selectedValue)
+  //   })
+  // }, [selectedValue])
 
-    storage.setAttributeToChange(value)
+  function changeAttributeValueHandler(value) {
+    changeElementsAttribute(value)
+
+    state.setSelectedValue(value)
+    storage.setAttributeToChangeValue(value)
   }
 
   return (
@@ -59,7 +101,7 @@ export default props => {
             value
             key={value}
             className="values-list-item"
-            onClick={() => changeAttributeValue(value)}
+            onClick={() => changeAttributeValueHandler(value)}
           >
             {value.toUpperCase()}
           </div>
