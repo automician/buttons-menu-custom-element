31a8ab5a601c5e76149bc9bdbc639c822c5b8752
ArtemPaskall <artempaskal@gmail.com>
2023-03-28 16:25:35 +0300

    styles props was added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ButtonsMenu from './ButtonsMenu'
 
 register(ButtonsMenu, 'buttons-menu', React, ReactDOM)
 
-register(({ rootElement, children }) => {
+register(({ rootElement, children, style }) => {
   rootElement.style.position = 'fixed'
   rootElement.style.right = '15px'
   rootElement.style.top = '15px'
@@ -43,7 +43,7 @@ register(
 )
 
 register(
-  ({ children, rootElement }) => {
+  ({ children, rootElement, style }) => {
     const {
       attributes: {
         grow: { value: grow },
