e842882bf16812fbaa7abfd8665a80686ab47d80
ArtemPaskall <artempaskal@gmail.com>
2023-03-29 21:53:44 +0300

    free spaces in valuesList bug fixed

--- src/ButtonsMenu.js
+++ src/ButtonsMenu.js
@@ -6,7 +6,7 @@ export default props => {
 
   const selectorOfElementsToChange = attributes['change-selector'].value
   const attributeToChange = attributes['change-attribute'].value
-  const valuesList = attributes['values'].value.split(',')
+  const valuesList = attributes['values'].value.split(',').trim()
   const maybeAskedDefaultValue = attributes.default?.value
 
   const maybeContainerizedContentLoadedEvent = attributes['on']?.value
