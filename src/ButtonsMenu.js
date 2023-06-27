e961b9e6645a6bb678658c17fb7eff39551340cc
ArtemPaskall <artempaskal@gmail.com>
2023-06-27 12:48:23 +0300

    buttons loaded console log added

--- src/ButtonsMenu.js
+++ src/ButtonsMenu.js
@@ -90,7 +90,8 @@ export default props => {
         .then(
           loaded => changeElementsAttribute(valueFromStorage),
           notLoaded => changeElementsAttribute(valueFromStorage),
-          document.dispatchEvent(new Event('FlexAndButtonsLoaded'))
+          document.dispatchEvent(new Event('FlexAndButtonsLoaded')),
+          () => console.log('ButtonsLoaded')
         )
         .catch(err => console.log(err))
     },
