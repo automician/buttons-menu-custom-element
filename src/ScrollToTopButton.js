b6cc81f4133a8ce3f9d2c54468004b667b61d6cf
Vladislav <48641751+ulissvl@users.noreply.github.com>
2023-10-23 23:34:34 +0300

    FEAT: arrow-up button hides when scroll down, changed style, always invisible in mobile version (#5)
    * feat: searchParams change atributes code and lang
    
    * changed to use attributeToChange variable and changed built files
    
    * added verifying if value from searchParams incluedes to valuesList
    
    * deleted builds
    
    * remove the button completely if there is search params
    
    * new build from 7 Oct 2023
    
    * FEAT: changed icon arrow_up button and added hide when croll is at top of window
    
    * deleted other icon and builds for resolve conflicts
    
    * new build from 8 oct 2023
    
    * added condition: if there isnt desktop don't show up button

--- src/ScrollToTopButton.js
+++ src/ScrollToTopButton.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ScrollToTopButtonStyles.scss'
 import ArrowImg from './images/arrow_up.png'
+import { isDesktop } from 'react-device-detect'
 
 export default () => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop
+    if (scrolled > 300) {
+      setVisible(true)
+    } else if (scrolled <= 300) {
+      setVisible(false)
+    }
+  }
+
+  if (isDesktop) {
+    window.addEventListener('scroll', toggleVisible)
+  }
+
   return (
     <div className="scroll-button">
       <img
@@ -10,6 +26,7 @@ export default () => {
         alt="scroll to top button"
         className="scroll-button-img"
         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        style={{ display: visible ? 'inline' : 'none' }}
       />
     </div>
   )
